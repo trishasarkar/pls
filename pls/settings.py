2244db7ed38e64c264902d72621ab0f99a97752c
Ambati Anirudh <ambatianirudh@Ambatis-MacBook-Air.local>
2021-05-26 14:17:50 +0530

    Update settings.py

--- pls/settings.py
+++ pls/settings.py
@@ -122,4 +122,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
