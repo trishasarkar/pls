f8d0c731780b691a964df1545177a7368fcf3902
Ambati Anirudh <ambatianirudh@Ambatis-MacBook-Air.local>
2021-05-26 13:03:54 +0530

    heroku

--- pls/settings.py
+++ pls/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pls.urls'
@@ -121,3 +122,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+django_heroku.settings(locals())
